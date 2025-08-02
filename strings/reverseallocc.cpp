8b584377113dd9ec56ffd0fc6bec0589f44d61df
Nova <beautymaurya9142@gmail.com>
2025-08-02 21:32:13 +0530

    Delete strings directory

--- strings/reverseallocc.cpp
+++
@@ -1,6 +0,0 @@
-string removeOccurrences(string s, string part) {
-        while(s.length()!=0&&s.find(part)<s.length()){
-            s.erase(s.find(part),part.length());
-        }
-        return s;
-    }
